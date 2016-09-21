d5f7fb87c2e4a7deda4b8be97639b50c58094e03
dpham147 <dannypham92@hotmail.com>
2016-09-21 00:21:56 -0700

    Polished XMLs

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/inandout/OrderActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/inandout/OrderActivity.java
@@ -1,13 +1,34 @@
 package edu.orangecoastcollege.cs273.dpham147.inandout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private EditText doubleDoubleEditText;
+    private EditText cheeseburgerEditText;
+    private EditText frenchFriesEditText;
+    private EditText shakesEditText;
+    private EditText smallDrinkEditText;
+    private EditText mediumDrinkEditText;
+    private EditText largeDrinkEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Order currentOrder = new Order();
+
+        doubleDoubleEditText = (EditText) findViewById(R.id.doubleDoubleEditText);
+        cheeseburgerEditText= (EditText) findViewById(R.id.cheeseburgerEditText);
+        frenchFriesEditText = (EditText) findViewById(R.id.frenchFriesEditText);
+        shakesEditText = (EditText) findViewById(R.id.shakesEditText);
+        smallDrinkEditText = (EditText) findViewById(R.id.smallDrinkEditText);
+        mediumDrinkEditText = (EditText) findViewById(R.id.mediumDrinkEditText);
+        largeDrinkEditText = (EditText) findViewById(R.id.largeDrinkEditText);
+
+
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/inandout/SummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/inandout/SummaryActivity.java
@@ -1,13 +1,24 @@
 package edu.orangecoastcollege.cs273.dpham147.inandout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    public TextView totalTextView;
+    public TextView itemsTextView;
+    public TextView subtotalTextView;
+    public TextView taxTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        itemsTextView = (TextView) findViewById(R.id.itemsTextView);
+        subtotalTextView = (TextView) findViewById(R.id.subtotalTextView);
+        taxTextView = (TextView) findViewById(R.id.taxTextView);
     }
 }
